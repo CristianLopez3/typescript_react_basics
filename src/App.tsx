7afd32cd8c7494d46b4d02befcbedc3ceccc5efb
Cristian <cristian.c.lopez.m@gmail.com>
2024-01-30 22:20:20 -0500

    basics

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import CourseGoal from "./components/CourseGoal";
+
+function App() {
+  return (
+    <main>
+      <CourseGoal
+        title="Learng React + TypeScript"
+      >
+        <p>Became a great react dev with typescript</p>
+      </CourseGoal>
+    </main>
+  );
+}
+
+export default App;
---
+++ src/components/CourseGoal.tsx
@@ -0,0 +1,22 @@
+import { type PropsWithChildren, type ReactNode } from "react";
+
+// interface CourseGoalProps {
+//   title: string;
+//   children: ReactNode;
+// }
+
+type CourseGoalProps = PropsWithChildren<{title: string}>;
+
+function CourseGoal({ title, children }: CourseGoalProps) {
+  return (
+    <article>
+      <div>
+        <h2>{title}</h2>
+        {children}
+      </div>
+      <button>Delete</button>
+    </article>
+  );
+}
+
+export default CourseGoal;
