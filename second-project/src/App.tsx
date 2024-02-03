1ac895bc69a47ce00586235ead7fe1ebd1d9be47
Cristian <cristian.c.lopez.m@gmail.com>
2024-02-02 23:19:27 -0500

    components editable

--- second-project/src/App.tsx
+++ second-project/src/App.tsx
@@ -1,10 +1,12 @@
-import './App.css';
+import Input from './components/Input';
 
 function App() {
 
   return (
     <>
       <h1>Let's get started</h1>
+      <Input label='Your name' id='name' type='text' />
+      <Input label='Your age' id='number' type='number' />
     </>
   )
 }
---
+++ second-project/src/components/Input.tsx
@@ -0,0 +1,23 @@
+import { ComponentPropsWithoutRef } from "react";
+
+type InputProps = {
+  label: string;
+  id: string;
+} & ComponentPropsWithoutRef<'input'>;
+
+function Input({label, id, ...props}: InputProps){
+
+  return(
+    <p>
+      <label htmlFor={id}>{label}</label>
+      <input 
+        id={id}
+        type="text"
+        {...props}
+      />
+    </p>
+  )
+
+}
+
+export default Input;
